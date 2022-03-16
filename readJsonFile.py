a116ea7ae92f3328a69746eec880236cbdcbcc81
gaurav tiwari <62843232+GITIWARII@users.noreply.github.com>
2022-03-16 18:05:41 +0530

    Update readJsonFile.py

--- readJsonFile.py
+++ readJsonFile.py
@@ -10,3 +10,23 @@ spark = SparkSession.builder.appName("readJsonFile").master("local").getOrCreate
 df = spark.read.json("C:\\Users\\Dell\\Downloads\\jsonfile\\sample2.json").cache()
 df.printSchema()
 df.show(5, truncate=False)
+
+
+
+
+'''
+{
+   "firstName": "Joe",
+   "lastName": "Jackson",
+   "gender": "male",
+   "age": 28,
+   "address": {
+       "streetAddress": "101",
+       "city": "San Diego",
+       "state": "CA"
+   },
+   "phoneNumbers": [
+       { "type": "home", "number": "7349282382" }
+   ]
+}
+'''
