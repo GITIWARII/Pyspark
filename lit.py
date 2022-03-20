ac3d6cd2830846685512794f8e12db2aaf59b46a
gaurav tiwari <62843232+GITIWARII@users.noreply.github.com>
2022-03-20 14:54:05 +0530

    Update lit.py

--- lit.py
+++ lit.py
@@ -4,7 +4,7 @@ findspark.init()
 from pyspark.sql import SparkSession
 
 
-spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+spark = SparkSession.builder.appName('usingLit').getOrCreate()
 data = [("111",50000),("222",60000),("333",40000)]
 columns= ["EmpId","Salary"]
 df = spark.createDataFrame(data = data, schema = columns)
